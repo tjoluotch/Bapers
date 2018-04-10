75d1eabbf0c7ee6b89bcd8c4df414a8611126a83
Daniel McKinley <SpartanHedgey@gmail.com>
2018-04-10 22:26:37 +0100

    Revert "Fixed import for task"
    This reverts commit 2bb294f46caeb6f54948911953292cecb255d8bc.

--- src/domain/Task.java
+++ src/domain/Task.java
@@ -19,7 +19,6 @@ import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
-import javax.persistence.Version;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import javax.xml.bind.annotation.XmlRootElement;
