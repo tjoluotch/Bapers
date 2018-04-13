fa16425b4d73b0f223efe7a3f391197c475d059f
slydogg <Sylvester.Mbagwu@city.ac.uk>
2018-04-13 21:35:16 +0100

    Manually add Daniels BAPPAYM class

--- src/Controllers/BAPPAYM.java
+++ src/Controllers/BAPPAYM.java
@@ -5,10 +5,75 @@
  */
 package Controllers;
 
+import data.DataManagerImpl;
+import domain.JobLine;
+import domain.OrderTable;
+import domain.PaymentDetail;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import java.util.Date;
+import javax.swing.JFrame;
+
 /**
  *
  * @author Daniel
  */
 public class BAPPAYM {
     
+    DataManagerImpl dm;
+    JFrame frame;
+    
+    public BAPPAYM(DataManagerImpl dm){
+        this.dm = dm;
+    }
+    
+    public BAPPAYM(DataManagerImpl dm, JFrame frame){
+        this.dm = dm;
+        this.frame = frame;
+    }
+  
+    
+    
+    //CREATE PAYMENTDETAIL METHODS
+    //card payments
+    //for whole order
+    public void createPayment(OrderTable order, String last4, Date expiryDate){
+        PaymentDetail payment = new PaymentDetail();
+        payment.setType("Card");
+        payment.setExpiryDate(expiryDate);
+        payment.setLast4digits(last4);
+        payment.setOrderID(order);
+        payment.setJobLineCollection(order.getJobLineCollection());
+    }
+    
+    //for one job
+    public void createPayment(JobLine job, String last4, Date expiryDate){
+        PaymentDetail payment = new PaymentDetail();
+        payment.setType("Card");
+        payment.setExpiryDate(expiryDate);
+        payment.setLast4digits(last4);
+        payment.addJobLine(job);
+        payment.setOrderID(job.getOrderID());
+    } 
+    
+    //cash payments
+    //for whole order
+    public void createPayment(OrderTable order){
+        PaymentDetail payment = new PaymentDetail();
+        payment.setType("cash");
+        payment.setExpiryDate(null);
+        payment.setLast4digits(null);
+        payment.setOrderID(order);
+        payment.setJobLineCollection(order.getJobLineCollection());
+    }
+    //for one job
+    public void createPayment(JobLine job){
+        PaymentDetail payment = new PaymentDetail();
+        payment.setType("cash");
+        payment.setExpiryDate(null);
+        payment.setLast4digits(null);
+        payment.setOrderID(job.getOrderID());
+        payment.addJobLine(job);
+    }
 }
+
