cc64a5f8ed9c689562bd31a3e3c09307c73f753d
slydogg <Sylvester.Mbagwu@city.ac.uk>
2018-04-16 02:19:30 +0100

    -

--- src/TableModels/OrderTableModel.java
+++ src/TableModels/OrderTableModel.java
@@ -5,10 +5,65 @@
  */
 package TableModels;
 
+import domain.OrderTable;
+import java.util.List;
+import javax.swing.table.AbstractTableModel;
+
 /**
  *
  * @author Tweetie Pie
  */
-public class OrderTableModel {
+public class OrderTableModel extends AbstractTableModel{
+    
+    @Override
+    public Class<?> getColumnClass(int columnIndex) {
+        switch (columnIndex){
+             case 0:
+               return String.class;
+             case 1:
+               return String.class;
+             case 2:
+               return String.class;
+             }
+             return null;
+         //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    private List<OrderTable> li;
+    private String[] columnNames = {"Order ID", "Total Price", "Payment Status"};
+
+    public OrderTableModel(List<OrderTable> li) {
+        this.li = li;
+    }
+
+    @Override
+    public String getColumnName(int column) {
+        return columnNames[column]; //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int getRowCount() {
+       return li.size(); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int getColumnCount() {
+        return 3; //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        OrderTable o = li.get(rowIndex);
+        switch(columnIndex){
+            case 0:
+                return o.getOrderID();
+            case 1:
+                return o.getTotalPrice();
+            case 2:
+                return o.getPaymentStatus();
+        }
+        
+       return null;
+    }
     
 }
